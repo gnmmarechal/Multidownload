c408cfa0339cd57662815c99edd7aa605b983dd5
Pirater12 <pokemonemerald0@gmail.com>
2016-07-08 14:45:43 +0530

    Update and rename main.c to main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -11,7 +11,8 @@ Result http_download(httpcContext *context, const char* url)//This error handlin
 	u32 statuscode=0;
 	u32 size=0, contentsize=0;
 	u8 *buf;
-
+                char *a=strrchr(url , '/');
+                  char *b=strrchr(a ,'.');
 	ret = httpcBeginRequest(context);
 	if(ret!=0)return ret;
 
@@ -29,18 +30,18 @@ Result http_download(httpcContext *context, const char* url)//This error handlin
 	buf = (u8*)malloc(contentsize);
 	if(buf==NULL)return -1;
 	memset(buf, 0, contentsize);
-
-
+                
 	ret = httpcDownloadData(context, buf, contentsize, NULL);
               Handle fileHandle;
 			u32 bytesWritten;
 			FS_Archive sdmcArchive=(FS_Archive){ARCHIVE_SDMC, (FS_Path){PATH_EMPTY, 1,                                               (u8*)""}};
-			FS_Path filePath=fsMakePath(PATH_ASCII,strrchr(url, '/'));
+                                                //strcat (c,a);
+			FS_Path filePath=fsMakePath(PATH_ASCII,a);
 			FSUSER_OpenFileDirectly( &fileHandle, sdmcArchive, filePath, FS_OPEN_CREATE|                                                FS_OPEN_WRITE, 0x00000000);
 			FSFILE_Write(fileHandle, &bytesWritten, 0, buf, contentsize,0x10001);
 			FSFILE_Close(fileHandle);
 			svcCloseHandle(fileHandle);
-			free(buf);
+			//free(buf);
                                              if(ret!=0)
 	{
 		free(buf);
@@ -92,11 +93,15 @@ int main(int argc, char **argv)
                 std::string InputHBKB = sHBKB.HBKB_CheckKeyboardInput(); // Check Input
 		
                  char* gba= const_cast<char*>(InputHBKB.c_str());
-                  //printf("Downloading %s\n",gba);
+                  //const char* gba= InputHBKB.c_str();
+                   printf("\x1b[3;0HDownload link :");
 
+                   printf("\x1b[4;0H%s", gba);
+                   //char* nds = strdup(gba);
+                  gfxFlushBuffers();
                 
                 if (KBState == 1) // User finished Input
-                 {printf("Downloading %s\n",gba);		
+                {		
                 ret = httpcOpenContext(&context,gba, 1);
 	printf("return from httpcOpenContext: %"PRId32"\n",ret);
 	gfxFlushBuffers();
@@ -105,6 +110,7 @@ int main(int argc, char **argv)
 	{
 		ret=http_download(&context , gba);
 		printf("return from http_download: %08"PRIx32"\n",ret);
+                                printf("Downloaded:%s\n",gba);
 		gfxFlushBuffers();
 		httpcCloseContext(&context);
 	}
