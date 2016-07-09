f52fa5e5ddb2e8d6ba04672f78cddc9c944e9d3b
Pirater12 <pokemonemerald0@gmail.com>
2016-07-10 01:04:40 +0530

    Code breakup

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,62 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <inttypes.h>
-Result http_download(httpcContext *context, const char* url)//This error handling needs updated with proper text printing once ctrulib itself supports that.
-{
-	Result ret=0;
-	u32 statuscode=0;
-	u32 size=0, contentsize=0;
-	u8 *buf;
-                char *a=strrchr(url, '/');
-                  char *b=strrchr(a ,'.');
-	//httpcAddRequestHeaderField(context, (char*)"User-Agent",  (char*)"MULTIDOWNLOAD");
-                //httpcAddTrustedRootCA(context, cybertrust_cer, cybertrust_cer_len);
-	//httpcAddTrustedRootCA(context, digicert_cer, digicert_cer_len);
-	ret = httpcBeginRequest(context);
-	if(ret!=0)return ret;
-                ret = httpcGetResponseStatusCode(context, &statuscode, 0);
-	if(ret!=0)return ret;
-                if(statuscode!=200)return -2;
-                ret=httpcGetDownloadSizeState(context, NULL, &contentsize);
-	if(ret!=0)return ret;
-
-	printf("size: %08"PRIx32"\n",contentsize);
-	gfxFlushBuffers();
-
-	buf = (u8*)malloc(contentsize);
-	if(buf==NULL)return -1;
-	memset(buf, 0, contentsize);
-                
-	ret = httpcDownloadData(context, buf, contentsize, NULL);
-              Handle fileHandle;
-			u32 bytesWritten;
-                                              //strcat (c,a);
-			FS_Path filePath=fsMakePath(PATH_ASCII,a);
-                                                FSUSER_OpenFileDirectly(&fileHandle, FS_ArchiveID::ARCHIVE_SDMC,
-                                                fsMakePath(FS_PathType::PATH_EMPTY, ""),
-                                                filePath, FS_OPEN_CREATE|   FS_OPEN_WRITE,0x00000000 );
-			FSFILE_Write(fileHandle, &bytesWritten, 0, buf, contentsize,0x10001);
-			FSFILE_Close(fileHandle);
-			svcCloseHandle(fileHandle);
-			//free(buf);
-                                             if(ret!=0)
-	{
-		free(buf);
-		return ret;
-	}
-
-	gfxFlushBuffers();
-	gfxSwapBuffers();
-	gspWaitForVBlank();
-
-	free(buf);
-
-	return 0;
-}
-
-
-
-
+#include "download.h"
 static SwkbdCallbackResult MyCallback(void* user, const char** ppMessage, const char* text, size_t textlen)
 {
 	if (strstr(text, "lenny"))
@@ -126,7 +71,7 @@ int main(int argc, char **argv)
 		                                       gfxFlushBuffers();
 		                                       httpcCloseContext(&context);
                                                            }
-
+                                                                
 			} 
 		}
                          
@@ -136,7 +81,7 @@ int main(int argc, char **argv)
 
 		gspWaitForVBlank();
 	}
-               httpcExit();
+                httpcExit();
 	gfxExit();
 	return 0;
 }
