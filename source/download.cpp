0aae3552b30c93175c9d61e085fa080af6d2cbc3
Pirater12 <pokemonemerald0@gmail.com>
2016-07-16 00:26:16 +0530

    Fix the 'md' bug

--- source/download.cpp
+++ source/download.cpp
@@ -6,16 +6,15 @@
 //extern char* newurl;
 Result http300(char*nurl)
 {
-  httpcInit(0);
-  gfxInitDefault();
-  consoleInit(GFX_TOP, NULL);
+
    Result ret=0;
    u8* buf;
    char *d=strrchr(nurl, '/');
+    char *a=strrchr(d,'.');
    u32 size=0;
    u32 statuscode=0;
    httpcContext context;
-   printf("Download:%s\n",nurl);
+   printf( "Download:%s\n ",nurl);
    ret = httpcOpenContext(&context, HTTPC_METHOD_GET, nurl, 0);
         if(ret!=0)return ret;
     printf("return from httpcOpenContext: %"PRId32"\n",ret);
@@ -27,9 +26,9 @@ Result http300(char*nurl)
         if(ret!=0)return ret;
    ret = httpcGetResponseStatusCode(&context, &statuscode, 0);
         if(ret!=0)return ret;
-      printf("Statuscode:%"PRId32"\n",statuscode);
+      printf(" Statuscode:%"PRId32"\n ",statuscode);
        if(statuscode==301||statuscode==302)
-      { printf("300\n");
+      { printf(" 300\n ");
         char newurl[1024];
         httpcGetResponseHeader(&context, (char*)"Location", newurl,1024);
         httpcCloseContext(&context);
@@ -38,7 +37,7 @@ Result http300(char*nurl)
    if(statuscode!=200)return -2;
    ret=httpcGetDownloadSizeState(&context, NULL, &size);
        if(ret!=0)return ret;
-     printf("size: %"PRId32"\n",size);
+     printf(" size: %"PRId32"\n ",size);
        gfxFlushBuffers();
      buf = (u8*)malloc(size);
        if(buf==NULL)return -1;
@@ -47,28 +46,16 @@ Result http300(char*nurl)
              Handle fileHandle;
              u32 bytesWritten;
                                               //strcat (c,a);
-             FS_Path filePath=fsMakePath(PATH_ASCII,d);
-             FSUSER_OpenFileDirectly(&fileHandle, FS_ArchiveID::ARCHIVE_SDMC,
-             fsMakePath(FS_PathType::PATH_EMPTY, ""),
-             filePath, FS_OPEN_CREATE|FS_OPEN_WRITE,0x00000000 );
-             FSFILE_Write(fileHandle, &bytesWritten, 0, buf, size,0x10001);
-             FSFILE_Close(fileHandle);
-             svcCloseHandle(fileHandle);
+                FILE *fptr = fopen(d, "wb");
+	fwrite(buf, 1, size, fptr);
+	fclose(fptr);
 			//free(buf);
                                                                         if(ret!=0)
 	                                                    {
 		                                      free(buf);
                                                                      httpcCloseContext(&context);
-		                                      printf("Downloaded:%s\n",nurl);
+		                                      //printf(" Downloaded:%s\n ",nurl);
+                                                                      return ret;
 	                                                          }
-
-	                                                  gfxFlushBuffers();
-	                                                  gfxSwapBuffers();
-	                                                  gspWaitForVBlank();
-                                                                 
-	                                                  free(buf);
-
-	                                                   httpcExit();
-	                                                      gfxExit();
 	                                                     return 0 ;
                                                                      }
