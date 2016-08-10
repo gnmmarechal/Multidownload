90ef9f672280b3d7f5716fedb15a12884832caeb
Mário Alexandre Lopes Liberato <gnmpolicemata91@gmail.com>
2016-08-10 11:16:51 +0100

    Updated for indentation of code (#6)
    * Updated for identation.
    
    * Updated for identation.

--- source/download.cpp
+++ source/download.cpp
@@ -4,56 +4,65 @@
 #include <stdlib.h>
 #include <inttypes.h>
 //extern char* newurl;
-Result http300(char*nurl,char* e)
-{printf("\x1b[2J");
-Result ret=0;
-u8* buf;
-char *d=strrchr(nurl, '/');
-u32 size=0;
-u32 statuscode=0;
-httpcContext context;
-printf( "Download:%s\n ",nurl);
-ret = httpcOpenContext(&context, HTTPC_METHOD_GET, nurl, 0);
-if(ret!=0)return ret;
-printf("return from httpcOpenContext: %"PRId32"\n",ret);
-ret=httpcAddRequestHeaderField(&context, (char*)"User-Agent", (char*)"MULTIDOWNLOAD");
-if(ret!=0)return ret;
-ret = httpcSetSSLOpt(&context, 1<<9);
-if(ret!=0)return ret;
-ret = httpcBeginRequest(&context);
-if(ret!=0)return ret;
-ret = httpcGetResponseStatusCode(&context, &statuscode, 0);
-if(ret!=0)return ret;
-printf(" Statuscode:%"PRId32"\n ",statuscode);
-if(statuscode==301||statuscode==302)
-{ printf(" 300\n ");
-char newurl[1024];
-httpcGetResponseHeader(&context, (char*)"Location", newurl,1024);
-httpcCloseContext(&context);
-return http300(newurl , e);
-}
-if(statuscode!=200)return -2;
-ret=httpcGetDownloadSizeState(&context, NULL, &size);
-if(ret!=0)return ret;
-printf(" size: %"PRId32"\n ",size);
-gfxFlushBuffers();
-buf = (u8*)malloc(size);
-if(buf==NULL)return -1;
-memset(buf, 0, size);
-ret = httpcDownloadData(&context, buf, size, NULL);
-Handle fileHandle;
-u32 bytesWritten;
-strcat (e,d);
-FILE *fptr = fopen(e, "wb");
-fwrite(buf, 1, size, fptr);
-fclose(fptr);
-//free(buf);
-if(ret!=0)
+
+Result http300(char* nurl, char* e)
 {
-free(buf);
-httpcCloseContext(&context);
-//printf(" Downloaded:%s\n ",nurl);
-return ret;
-}
-return 0 ;
+    printf("\x1b[2J");
+    Result ret = 0;
+    u8* buf;
+    char* d = strrchr(nurl, '/');
+    u32 size = 0;
+    u32 statuscode = 0;
+    httpcContext context;
+    printf("Download:%s\n ", nurl);
+    ret = httpcOpenContext(&context, HTTPC_METHOD_GET, nurl, 0);
+    if (ret != 0)
+        return ret;
+    printf("return from httpcOpenContext: %" PRId32 "\n", ret);
+    ret = httpcAddRequestHeaderField(&context, (char*)"User-Agent", (char*)"MULTIDOWNLOAD");
+    if (ret != 0)
+        return ret;
+    ret = httpcSetSSLOpt(&context, 1 << 9);
+    if (ret != 0)
+        return ret;
+    ret = httpcBeginRequest(&context);
+    if (ret != 0)
+        return ret;
+    ret = httpcGetResponseStatusCode(&context, &statuscode, 0);
+    if (ret != 0)
+        return ret;
+    printf(" Statuscode:%" PRId32 "\n ", statuscode);
+    if (statuscode == 301 || statuscode == 302) {
+        printf(" 300\n ");
+        char newurl[1024];
+        httpcGetResponseHeader(&context, (char*)"Location", newurl, 1024);
+        httpcCloseContext(&context);
+        return http300(newurl, e);
+    }
+    if (statuscode != 200)
+        return -2;
+    ret = httpcGetDownloadSizeState(&context, NULL, &size);
+    if (ret != 0)
+        return ret;
+    printf(" size: %" PRId32 "\n ", size);
+    gfxFlushBuffers();
+    buf = (u8*)malloc(size);
+    if (buf == NULL)
+        return -1;
+    memset(buf, 0, size);
+    ret = httpcDownloadData(&context, buf, size, NULL);
+    Handle fileHandle;
+    u32 bytesWritten;
+    strcat(e, d);
+    FILE* fptr = fopen(e, "wb");
+    fwrite(buf, 1, size, fptr);
+    fclose(fptr);
+    //free(buf);
+    if (ret != 0) {
+        free(buf);
+        httpcCloseContext(&context);
+        //printf(" Downloaded:%s\n ",nurl);
+        return ret;
+    }
+    return 0;
 }
--- source/main.cpp
+++ source/main.cpp
@@ -1,106 +1,103 @@
 #include "libs.h"
 #include "download.h"
-int main() {
-SwkbdState swkbd;
-char mybuf[960];
-char loca[1024];
-SwkbdButton button = SWKBD_BUTTON_NONE;
-bool didit = false;
-bool didloc = false;
-char buffer[100];
- //Result ret=1;
-gfxInitDefault();
-PrintConsole topScreen, bottomScreen;
-consoleInit(GFX_TOP, NULL);
-FILE *file = fopen("multi.cfg","rb");
-if (file == NULL)
-{printf("Downloading to the root of the sdmc\n");
-fclose(file);
-//didloc = true;
-}
-else
+int main()
 {
-fseek(file,0,SEEK_END);
-off_t size = ftell(file);
-fseek(file,0,SEEK_SET);
-//if(!buffer)
-//printf("error while allocating buffer\n");
-off_t bytesRead = fread(buffer,1,size,file);
+    SwkbdState swkbd;
+    char mybuf[960];
+    char loca[1024];
+    SwkbdButton button = SWKBD_BUTTON_NONE;
+    bool didit = false;
+    bool didloc = false;
+    char buffer[100];
+    //Result ret=1;
+    gfxInitDefault();
+    PrintConsole topScreen, bottomScreen;
+    consoleInit(GFX_TOP, NULL);
+    FILE* file = fopen("multi.cfg", "rb");
+    if (file == NULL) {
+        printf("Downloading to the root of the sdmc\n");
+        fclose(file);
+        //didloc = true;
+    }
+    else {
+        fseek(file, 0, SEEK_END);
+        off_t size = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        //if(!buffer)
+        //printf("error while allocating buffer\n");
+        off_t bytesRead = fread(buffer, 1, size, file);
 
-fclose(file);
-if(size!=bytesRead)
-printf("error");
-}
-printf("MultiDownload by Kartik\n");
-printf("Press A to begin\n");
-printf("Press X to edit Download location\n");
-printf("Press START to exit\n");
-printf("Will download to location %s\n", buffer);
-strcpy(loca,buffer);
-while (aptMainLoop())
-{
-gspWaitForVBlank();
-hidScanInput();
-u32 kDown = hidKeysDown();
-if (kDown & KEY_X)
-{
-didloc = true;
-const char* texgen="Enter download to location";
-swkbdInit(&swkbd, SWKBD_TYPE_WESTERN, 2, -1);
-swkbdSetValidation(&swkbd, SWKBD_NOTEMPTY_NOTBLANK, 0, 0);
-swkbdSetHintText(&swkbd, texgen);
-button = swkbdInputText(&swkbd, loca, sizeof(loca));
-if(buffer[0] == '\0') {
-strcpy(buffer, loca);
-}	
-if ((didloc) && button != SWKBD_BUTTON_NONE)
-button = SWKBD_BUTTON_NONE;
-didit = false;
-   FILE *fp;
-   fp = fopen("multi.cfg", "wb");
-   fprintf(fp,loca);
-   fclose(fp);
-printf("Location changed to %s\n",loca);
-}
-if (kDown & KEY_A)
-{
-didit = true;
-const char* texgen2="Enter download url";
-swkbdInit(&swkbd, SWKBD_TYPE_WESTERN, 2, -1);
-swkbdSetHintText(&swkbd, texgen2);
-swkbdSetValidation(&swkbd, SWKBD_NOTEMPTY_NOTBLANK, 0, 0);
-button = swkbdInputText(&swkbd, mybuf, sizeof(mybuf));
-httpcInit(0);
-Result ret=1;
-if ((didit) && button != SWKBD_BUTTON_NONE)
-ret=http300(mybuf,loca);
-if(ret==0)
-{printf("Downloaded/n");
-}
-httpcExit();
-//cleaning vars
-for (int i = 0; i < 960; i++)
-mybuf[i] = ' ';
-button = SWKBD_BUTTON_NONE;
-didit = false;
-printf("\x1b[2J");
-printf("MultiDownload by Kartik\n");
-printf("Press A to begin\n");
-printf("Press X to edit Download location\n");
-printf("Press START to exit\n");
-if(buffer[0] == '\0') {
-printf("Downloading to the root of the sd card");
-}
-else {
-printf("Will download to location %s \n", buffer);
-}
-}
-if (kDown & KEY_START)
-break;
-// Flush and swap framebuffers
-gfxFlushBuffers();
-gfxSwapBuffers();
-}
-gfxExit();
-return 0;
+        fclose(file);
+        if (size != bytesRead)
+            printf("error");
+    }
+    printf("MultiDownload by Kartik\n");
+    printf("Press A to begin\n");
+    printf("Press X to edit Download location\n");
+    printf("Press START to exit\n");
+    printf("Will download to location %s\n", buffer);
+    strcpy(loca, buffer);
+    while (aptMainLoop()) {
+        gspWaitForVBlank();
+        hidScanInput();
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_X) {
+            didloc = true;
+            const char* texgen = "Enter download to location";
+            swkbdInit(&swkbd, SWKBD_TYPE_WESTERN, 2, -1);
+            swkbdSetValidation(&swkbd, SWKBD_NOTEMPTY_NOTBLANK, 0, 0);
+            swkbdSetHintText(&swkbd, texgen);
+            button = swkbdInputText(&swkbd, loca, sizeof(loca));
+            if (buffer[0] == '\0') {
+                strcpy(buffer, loca);
+            }
+            if ((didloc) && button != SWKBD_BUTTON_NONE)
+                button = SWKBD_BUTTON_NONE;
+            didit = false;
+            FILE* fp;
+            fp = fopen("multi.cfg", "wb");
+            fprintf(fp, loca);
+            fclose(fp);
+            printf("Location changed to %s\n", loca);
+        }
+        if (kDown & KEY_A) {
+            didit = true;
+            const char* texgen2 = "Enter download url";
+            swkbdInit(&swkbd, SWKBD_TYPE_WESTERN, 2, -1);
+            swkbdSetHintText(&swkbd, texgen2);
+            swkbdSetValidation(&swkbd, SWKBD_NOTEMPTY_NOTBLANK, 0, 0);
+            button = swkbdInputText(&swkbd, mybuf, sizeof(mybuf));
+            httpcInit(0);
+            Result ret = 1;
+            if ((didit) && button != SWKBD_BUTTON_NONE)
+                ret = http300(mybuf, loca);
+            if (ret == 0) {
+                printf("Downloaded/n");
+            }
+            httpcExit();
+            //cleaning vars
+            for (int i = 0; i < 960; i++)
+                mybuf[i] = ' ';
+            button = SWKBD_BUTTON_NONE;
+            didit = false;
+            printf("\x1b[2J");
+            printf("MultiDownload by Kartik\n");
+            printf("Press A to begin\n");
+            printf("Press X to edit Download location\n");
+            printf("Press START to exit\n");
+            if (buffer[0] == '\0') {
+                printf("Downloading to the root of the sd card");
+            }
+            else {
+                printf("Will download to location %s \n", buffer);
+            }
+        }
+        if (kDown & KEY_START)
+            break;
+        // Flush and swap framebuffers
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+    }
+    gfxExit();
+    return 0;
 }
