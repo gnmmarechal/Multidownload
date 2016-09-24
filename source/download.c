cced43a40f428b6e16c59c76f04bfac91b7ff4a9
Kartik <pokemonemerald0@gmail.com>
2016-09-24 19:17:59 +0530

    Fix stuff also some improvements to ui

--- source/download.c
+++ source/download.c
@@ -1,10 +1,51 @@
 #include "libs.h"
 #include "zip.h"
+#include <sys/time.h>
+#include "mega.h"
+
+u64 top=3000000000;
+
 
 Result http300(char *nurl);
-
-
-Result make(u8* buf,u32* size,char*loca)
+Result make(u8* buf,u32 size,char* loca);
+Result dsx(char *nurl,u8* buf, u32 size)
+{      const char *b = ".3dsx";
+	   const char *k ="/";  
+	   char* d = strrchr(nurl, '/');
+	   printf("%s",d);
+	   char *target =NULL;
+	   char *end ,*start;
+	   Result ret=0;
+	  if(start=strstr(nurl,k))
+	  {
+		  start +=strlen(k);
+		  if (end= strstr(start,b))
+		  {
+		  target = (char*)malloc(end-start+1);
+		  memcpy(target,start,end-start);
+		  target[end-start]= '\0';
+		  }
+		  else 
+			  printf("error\n");
+	        }
+			else
+			printf("error\n");
+		   char* g = strrchr(target, '/');
+          printf("%s\n",target);
+		   printf("%s\n",g);
+		   char a[1002]="/3ds";
+		  strcat(a,g);
+		  strcat(a,d);
+          ret = make(buf, size,a);
+		  if (ret==0)
+		  {   printf("%s\n",a);
+			  printf("success\n");
+		      free(target);
+		      return 0;
+			  }
+           return 0;
+}
+Result make(u8* buf,u32 size,char* loca)
 {
 FILE* fptr = fopen(loca, "wb");
       fwrite(buf, 1, size, fptr);
@@ -14,37 +55,35 @@ FILE* fptr = fopen(loca, "wb");
 
 Result http_down(char* nurl)
 { 
-	char* d = strrchr(nurl, '/');
+	 char* d = strrchr(nurl, '/');
     Result ret = 0;
     u32 statuscode=0;
 	u32 contentsize=0, readsize=0, size=0;
     u8 *buf, *lastbuf;
     httpcContext context;
-    printf("Download:%s\n", nurl);
-	
+    printf("\x1b[33;1mDownloading:%s\n\x1b[37;1m", nurl);
+	int ds = 0;
 	ret = httpcOpenContext(&context, HTTPC_METHOD_GET, nurl, 0);
     if (ret != 0)
-	{printf("error 0x%08X",ret);
-	return ret;
+	{printf("\x1b[31;1merror 0x%08X\n",ret);
+	return 1;
 	}
-    
-    printf("return from httpcOpenContext: %" PRId32 "\n", ret);
     ret = httpcAddRequestHeaderField(&context, (char*)"User-Agent", (char*)"MULTIDOWNLOAD");
     if (ret != 0){
-		 printf("error in ARHF 0x%08X",ret);
-		  return ret;
+		 printf("\x1b[31;1merror in ARHF 0x%08X\n",ret);
+		  return 1;
          }
     ret = httpcSetSSLOpt(&context, 1 << 9);
     if (ret != 0){
-		 printf("error in SSLO 0x%08X",ret);
-		  return -1;
+		 printf("\x1b[31;1merror in SSLO 0x%08X\n",ret);
+		  return 1;
          }
     ret = httpcBeginRequest(&context);
     if (ret != 0){
-		 printf("error in HPR 0x%08X",ret);
-		  return -1;
+		 printf("\x1b[31;1merror in HPR 0x%08X\x1b[37;1m\n",ret);
+		  return 1;
          }
-    ret = httpcGetResponseStatusCode(&context, &statuscode, 0);
+	ret = httpcGetResponseStatusCodeTimeout(&context, &statuscode,6000000000);
     if (ret != 0)
 	{printf("Wrong protocol:%s\n",nurl);
     return -1;
@@ -61,22 +100,24 @@ Result http_down(char* nurl)
         return -2;
     ret = httpcGetDownloadSizeState(&context, NULL, &contentsize);
     if (ret != 0){
-		 printf("error in HGDSS 0x%08X",ret);
-		  return ret;
+		 printf("\x1b[31;1merror in HGDSS 0x%08X\x1b[37;1m\n",ret);
+		  return 1;
          }
     printf("size(may be wrong): %" PRId32 "\n ",contentsize);
     gfxFlushBuffers();
 	gfxSwapBuffers();
     buf = (u8*)malloc(0x1000);
     if (buf == NULL)
+	{
+		printf("\x1b[31;1merror while setting buffer\n\x1b[37;1m");
         return -1;
     //memset(buf, 0, size);
-	
+	}
 	if(!(mkdir(loca , 0777)))
-		printf("Path didn't exist made one with success\n");
+		printf("\x1b[32;1 Path made with success\n\x1b[37;1m");
 	else 
 	{   if (ENOENT == errno)
-		{printf("error with path\n");
+		{printf("\x1b[31;1merror with path\n\x1b[37;1m");
 		}
 	     if (EEXIST == errno)
 		 { printf("exists\n");
@@ -97,15 +138,14 @@ Result http_down(char* nurl)
 				if(buf==NULL){ 
 					httpcCloseContext(&context);
 					free(lastbuf);
-				    return -1;
+				    return -2;
 					}
 			}
 	} while (ret == (s32)HTTPC_RESULTCODE_DOWNLOADPENDING);	
      if (ret!=0)
 	 {
-		 printf("\nDownload failed\n");
 		 httpcCloseContext(&context);
-		 return ret;
+		 return 1;
      }
     Handle fileHandle;
     u32 bytesWritten;
@@ -119,45 +159,16 @@ Result http_down(char* nurl)
 	if(buf==NULL){ // realloc() failed.
 		httpcCloseContext(&context);
 		free(lastbuf);
-	    return -1;
+	    return 1;
 		}
  	 char  f[1000];
 	 const char *git=  "https://github-cloud.s3.amazonaws.com";
-	 const char *b = ".3dsx";
-	 const char *k ="/";
 	 const char *git1="filename%3D";
 	 const char *git2="&";
 	 const char *gba="gbatemp.net";
 	 char *target =NULL;
 	 char *end ,*start;
-	 if(strstr(nurl,b)!=NULL)
-	 { printf("got 3dsx\n");
-      if(start=strstr(nurl,k))
-	  {
-		  start +=strlen(k);
-		  if (end= strstr(start,b))
-		  {
-		  target = (char*)malloc(end-start+1);
-		  memcpy(target,start,end-start);
-		  target[end-start]= '\0';
-		  }
-		  else 
-			  printf("error\n");
-	        }
-			else
-			printf("error\n");
-		   char* g = strrchr(target, '/');
-		  char a[100]="/3ds";
-		  strcat(a,g);
-		  strcat(a,d);
-          ret = make(buf, size,a);
-		  if (ret==0)
-			  printf("%s\n",a);
-			  printf("success\n");
-		      free(target);
-		  }
-		
-	else if(strstr(nurl,git)!=NULL)
+	if(strstr(nurl,git)!=NULL)
 	{printf("got git\n");
      
 	 if(start=strstr(nurl,git1))
@@ -170,10 +181,10 @@ Result http_down(char* nurl)
 		  target[end-start]= '\0';
 		  }
 		  else 
-			  printf("error\n");
+			  printf("\x1b[31;1merror\n\x1b[37;1m");
 	        }
 			else
-			printf("error\n");
+			printf("\x1b[31;1merror\n\x1b[37;1m");
 		  if (target)printf("Filename is %s\n",target);
 		 char f[1024]="/";
 		  strcat (f , target);
@@ -181,6 +192,36 @@ Result http_down(char* nurl)
 		  printf("location is %s\n",loca);
           ret = make(buf, size,loca);
 		  free(target);
+		  if(strstr(nurl,".3dsx")!=NULL)
+	   {  printf("got 3dsx\n");
+          printf("%s\n",f);
+		  char nu[1000];
+		  strcpy(nu,target);
+	      int len = strlen(target);
+          nu[len-5] = '\0';
+		  char f[1024]="/3ds/";
+		    strcat(f,nu);
+			if(!(mkdir(f , 0777)))
+		printf("\x1b[32;1 Path made with success\n\x1b[37;1m");
+	     else 
+	 {   if (ENOENT == errno)
+		{printf("\x1b[31;1merror with path\n\x1b[37;1m");
+		}
+	     if (EEXIST == errno)
+		 { printf("exists\n");
+	   }			 
+	} 
+			strcat(f,"/");
+			strcat(f,target);
+			
+			ret = make(buf, size,f);
+		  if (ret==0)
+		  {   
+			  printf("success\n");
+		      ds=1;
+			  return 0;
+			}
+	   }
 		  char *exton =strrchr(f , '.');
           if(strcmp(exton + 1,"zip") == 0) {
           printf("zip file\n");
@@ -189,11 +230,17 @@ Result http_down(char* nurl)
 		  ret = ezip(loca);
 		  if (ret==0)
 		    {   
-	          printf("Extracted\n");
+	          printf("\x1b[32;1mExtracted\n\x1b[37;1m");
 		      sdmcExit();
 		      fsExit();
 		    }
 		  }
+	    }
+		else if(ds==0){
+	   if(strstr(nurl,".3dsx")!=NULL)
+	   { printf("got 3dsx\n");
+	   ret = dsx(nurl,buf,size);
+	   }
 	    }
 		else if (strstr(nurl,gba)!=NULL)
 		{ 
@@ -233,7 +280,7 @@ Result http_down(char* nurl)
 		  ret = ezip(loca);
 		  if (ret==0)
 		    {   
-	          printf("Extracted\n");
+	          printf("\x1b[31;1mExtracted\n\x1b[37;1m");
 		      sdmcExit();
 		      fsExit();
 		    }
@@ -256,7 +303,7 @@ Result http_down(char* nurl)
 		  ret = ezip(loca);
 		  if (ret==0)
 		    {   
-	          printf("Extracted\n");
+	          printf("\x1b[32;1mExtracted\n\x1b[37;1m");
 		      sdmcExit();
 		      fsExit();
 		    }
@@ -271,7 +318,15 @@ Result http_down(char* nurl)
 Result http300(char *nurl)
 {  
    Result ret=0;
-
+   if (strstr(nurl,"mega")!=NULL)
+   {
+	   ret=doMegaInstall(nurl);
+	   if (ret!=3)
+	   return 1;
+       else 
+	   return 0;
+   }
+   
    if(strstr(nurl,"http")!=NULL)
    {
 	  ret= http_down(nurl);
@@ -287,5 +342,13 @@ Result http300(char *nurl)
     ret=http_down(b);	
 	 } 
    }
+  memset(loca, 0, 1024);   //solve error not the correct way to solve this but works.
+  extern char buffer[1024];
+  strcpy(loca,buffer);
+  if (ret==1)
+  {
+	  return 1;
+  }
+    
   return 0;
 }
